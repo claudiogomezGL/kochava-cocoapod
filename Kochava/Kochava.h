60b889acafe2d94ec9026a1b6c6ae0a5e83bdcdd
Claudio Gomez <claudio.gomez@gorillalogic.com>
2017-11-23 11:13:46 -0600

    Updated project configuration.

---
+++ Kochava/Kochava.h
@@ -0,0 +1,17 @@
+//
+//  Kochava.h
+//  Kochava
+//
+//  Created by Claudio Gomez on 11/23/17.
+//  Copyright © 2017 Claudio Gomez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kochava.
+FOUNDATION_EXPORT double KochavaVersionNumber;
+
+//! Project version string for Kochava.
+FOUNDATION_EXPORT const unsigned char KochavaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kochava/PublicHeader.h>
